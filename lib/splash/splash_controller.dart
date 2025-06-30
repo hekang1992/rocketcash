0b0c5b91939ad49936e1cd4bd661d79eb5be102e
何康 <hekang19921111@hotmail.com>
2025-07-01 00:17:58 +0800

    ref:优化

--- lib/splash/splash_controller.dart
+++ lib/splash/splash_controller.dart
@@ -1,17 +1,12 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/state_manager.dart';
 import 'package:rocketcash/hive/save_info.dart';
 import 'package:rocketcash/routes/routes.dart';
 
 class SplashController extends GetxController {
   final Connectivity _connectivity = Connectivity();
-  late final Stream<ConnectivityResult> _connectivityStream;
   final RxString connectionStatus = ''.obs;
 
   @override
